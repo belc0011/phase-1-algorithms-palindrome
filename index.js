bdd3911f733127a31b2e2c2c0c5be4b7ad857dab
Monica Garcia <monicairenegarcia@gmail.com>
2023-12-26 14:53:18 -0700

    completed algorithms palindrome lab

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let palindrome = false;
+  for (let i = 0; i < word.length; i++) {
+    if (word.charAt(i) === word.charAt(word.length - 1 - i)) {
+      palindrome = true
+    }
+    else break;
+  }
+  return palindrome;
 }
 
 /* 
-  Add your pseudocode here
+ Create function "isPalindrome" that takes in a word and outputs a Boolean
+ Initialize boolean variable to false
+ Words will be different lengths, so cannot hard code numbers
+ Use for loop and set length of loop to be less than word length
+ Inside for loop, compare character at i to character at word length -1 - i
+ If equal, set boolean to true, else break from for loop
+ Return boolean
 */
 
 /*
-  Add written explanation of your solution here
+My code is optimized because I do not use many if statements to account
+for different length words - I instead use one for loop that uses
+the length of the word to determine how many characters it needs to
+compare. The function name and variable name are descriptive so that
+someone else looking at the code would understand their function.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("baba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("babababab"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
